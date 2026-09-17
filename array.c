928f2d712aa6bcaf3669a6985abf6917aa766618
Chamath-ccsk <chamathchamikara2556@gmail.com>
2026-09-17 20:58:04 +0530

    array

---
+++ array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+
+int x;
+for (int x=1;x<10;x++){
+  if(x%7==0){
+    goto label1;
+
+    }
+  printf("%d\n",x);
+  }
+   label1:
+     printf("%d is devided by 7",x);
+
+
+return 0; 
+}
+
